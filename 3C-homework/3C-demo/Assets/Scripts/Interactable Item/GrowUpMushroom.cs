80064f8015368dadee62233d40803762316cf48c
Tingyu Peng <tingyupeng@126.com>
2020-10-08 20:49:21 +0800

    init

---
+++ 3C-homework/3C-demo/Assets/Scripts/Interactable Item/GrowUpMushroom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/GrowUpMushroom")]
+public class GrowUpMushroom : Item
+{
+    public override void Use()
+    {
+        base.Use();
+        GameObject player = GameObject.Find("Player");
+        Vector3 curScale = player.GetComponent<Transform>().localScale;
+
+        // player.GetComponentInChildren<Animation>().Play("jump-up");
+
+        player.GetComponent<Transform>().localScale = 1.1f * curScale;
+        RemoveFromInventory();
+
+    }
+
+}
